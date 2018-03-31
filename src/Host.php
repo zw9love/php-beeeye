ef367c3989c1d75110dc713b23068fa3ceff64f9
zw9love <415789992@qq.com>
2018-03-31 17:45:47 +0800

    finish host api

--- src/Host.php
+++ src/Host.php
@@ -18,31 +18,42 @@ header('Access-Control-Allow-Origin: http://localhost:8000');
 // echo $_GET['jsoncallback'] . "(".json_encode($arr).")";
 require("./Login.php");
 $query = $_SERVER['QUERY_STRING'];
+//var_dump($_SERVER);
+
+$sqlInfo = Array(
+    "serverName" => "localhost",
+    "username" => "root",
+    "password" => "159357"
+);
+
 switch ($query) {
     case "get":
-        get();
+        get($sqlInfo);
         break;
     case "post":
-        post();
+        post($sqlInfo);
+        break;
+    case "put":
+        put($sqlInfo);
+        break;
+    case "delete":
+        delete($sqlInfo);
         break;
 }
 
-function get()
+function get($sqlInfo)
 {
     $final = file_get_contents('php://input');
     $data = json_decode($final, true);
-    $page = $data["page"];
     try {
 //        echo md5($data["login_pwd"]);
-        $servername = "localhost";
-        $username = "root";
-        $password = "159357";
-        $pdo = new PDO("mysql:host=$servername;dbname=beeeyehced", $username, $password);
+        $pdo = new PDO("mysql:host=".$sqlInfo["serverName"].";dbname=beeeyehced", $sqlInfo["username"], $sqlInfo["password"]);
         //查询
-        $sql = "";
         $pageNumber = 0;
         $pageSize = 30;
-        if (count($page) > 0) {
+        //  && count($page) > 0
+        if (!is_null($data["page"])) {
+            $page = $data["page"];
             $pageNumber = $page['pageNumber'] - 1;
             $pageSize = $page["pageSize"];
         }
@@ -77,8 +88,84 @@ function get()
     }
 }
 
-function post()
+function post($sqlInfo)
 {
+    $final = file_get_contents('php://input');
+    $data = json_decode($final, true);
+    try {
+        $pdo = new PDO("mysql:host=".$sqlInfo["serverName"].";dbname=beeeyehced", $sqlInfo["username"], $sqlInfo["password"]);
+        $sql = "INSERT INTO beeeye_host (host_ids, name, ip, port, os_type, os_version, os_arch, login_name, login_pwd, status) VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        $stmt = $pdo->prepare($sql);
+        $hostIds = getRandomString();
+        $stmt->bindValue(1, $hostIds);
+        $stmt->bindValue(2, $data["name"]);
+        $stmt->bindValue(3, $data["ip"]);
+        $stmt->bindValue(4, $data["port"]);
+        $stmt->bindValue(5, $data["os_type"]);
+        $stmt->bindValue(6, $data["os_version"]);
+        $stmt->bindValue(7, $data["os_arch"]);
+        $stmt->bindValue(8, $data["login_name"]);
+        $stmt->bindValue(9, $data["login_pwd"]);
+        $stmt->bindValue(10, 0);
+        $execRet = $stmt->execute();
+        $row_count = $stmt->rowCount();
+        $pdo = null;
+        if($row_count > 0){
+            echo getJsonData(200, "成功", null);
+        }else{
+            echo getJsonData(606, "失败", null);
+        }
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+    }
+}
 
+function put($sqlInfo)
+{
+    $final = file_get_contents('php://input');
+    $data = json_decode($final, true);
+    try {
+        $pdo = new PDO("mysql:host=".$sqlInfo["serverName"].";dbname=beeeyehced", $sqlInfo["username"], $sqlInfo["password"]);
+        $sql = "UPDATE beeeye_host SET name = ?, ip = ?, port = ?,login_name = ?,login_pwd = ? where host_ids = ?";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindValue(1, $data["name"]);
+        $stmt->bindValue(2, $data["ip"]);
+        $stmt->bindValue(3, $data["port"]);
+        $stmt->bindValue(4, $data["login_name"]);
+        $stmt->bindValue(5, $data["login_pwd"]);
+        $stmt->bindValue(6, $data["host_ids"]);
+        $execRet = $stmt->execute();
+        $row_count = $stmt->rowCount();
+        $pdo = null;
+        if($row_count > 0){
+            echo getJsonData(200, "成功", null);
+        }else{
+            echo getJsonData(606, "失败", null);
+        }
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+    }
+}
+
+function delete($sqlInfo)
+{
+    $final = file_get_contents('php://input');
+    $data = json_decode($final, true);
+    try {
+        $pdo = new PDO("mysql:host=".$sqlInfo["serverName"].";dbname=beeeyehced", $sqlInfo["username"], $sqlInfo["password"]);
+        $sql = "DELETE FROM beeeye_host where host_ids = ?";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindValue(1, $data["host_ids"]);
+        $execRet = $stmt->execute();
+        $row_count = $stmt->rowCount();
+        $pdo = null;
+        if($row_count > 0){
+            echo getJsonData(200, "成功", null);
+        }else{
+            echo getJsonData(606, "失败", null);
+        }
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+    }
 }
 
